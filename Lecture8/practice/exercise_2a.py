0a4f18367952e9417fc269b7ade76488e3e4e9c8
nptchang <npt.chang@gmail.com>
2018-04-11 22:17:58 +0800

    updated

---
+++ Lecture8/practice/exercise_2a.py
@@ -0,0 +1,13 @@
+def fancy_divide(numbers,index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        print("-1")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+fancy_divide([0,2,4], 4)
---
+++ Lecture8/practice/exercise_2e.py
@@ -0,0 +1,13 @@
+def fancy_divide(list_of_numbers, index):
+    try:
+        try:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+        finally:
+            raise Exception("0")
+    except Exception as ex:
+        print(ex)
+
+
+fancy_divide([0, 2, 4], 0)
